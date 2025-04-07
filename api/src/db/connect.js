261e4c0c045345e72c8f08f635f228c29921cec5
MateusC2 <mateuscubeiro.educ@gmail.com>
2025-04-07 16:26:24 -0300

    atualização

--- api/src/db/connect.js
+++ api/src/db/connect.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
     connectionLimit:10,
     host:'localhost',
     user:'alunods',
-    password:'senai@604',
+    password:'mate123',
     database:'vio_mateus'
 })
 
